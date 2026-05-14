e76a9c16260a1b046dd464551a9e7e893dee3551
Cauã Araújo Bernardes <cauabernardess@gmail.com>
2026-05-14 08:55:41 -0300

    Restore Django settings file with initial configuration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,7 +114,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'saep',
         'USER': 'root',
-        'PASSWORD': 'senai',
+        'PASSWORD': '************',
         'HOST': '127.0.0.1',
         'PORT': '3306',
 #         # 'OPTIONS': {
